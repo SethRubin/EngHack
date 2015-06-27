5caa530aa5677b0d179cee0d75a639062c80b140
Allen Wang <allen.houze.wang@gmail.com>
2015-06-27 04:20:27 -0400

    Added cron job using ApScheduler

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EngHack
+
+App that notifies user when their subscribed words are trending
+
--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 from flask import Flask
 import json
+
+from bulk_email import bulk_email
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,17 +15,13 @@ def hello_world():
     }
     return json.dumps(obj)
 
-@app.route('/word')
-def hello_world():
-    obj = {
-        'key': 3,
-        'key2': 5,
-        'key3': 6 
-    }
-    return json.dumps(obj)
+sendEmail = BackgroundScheduler(daemon=True)
+sendEmail.start()
 
-
-
+@sendEmail.scheduled_job('interval', seconds=10)
+def timed_job():
+    bulk_email('yo cron', ['h353wang@uwaterloo.ca', 'seth.h.rubin@gmail.com'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
+
