abff43cdb2fc23aafbe59bd2c556f3102d452ab2
Allen Wang <allen.houze.wang@gmail.com>
2015-06-27 02:26:56 -0400

    save

--- hello.py
+++ hello.py
@@ -1,13 +1,27 @@
-import os
 from flask import Flask
-
+import json
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'YOOOO HOSJIASJIAJ'
+def hello_world():
+    obj = {
+        'key': 3,
+        'key2': 5,
+        'key3': 6 
+    }
+    return json.dumps(obj)
+
+@app.route('/word')
+def hello_world():
+    obj = {
+        'key': 3,
+        'key2': 5,
+        'key3': 6 
+    }
+    return json.dumps(obj)
 
 
-@app.route('/page')
-def newhello():
-    return 'sasda'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
